d2fca8f01d302f6daaf3a2627061e0f408aa6410
K <kay-is@users.noreply.github.com>
2023-03-07 10:18:48 +0000

    ComposeDB: Add user session for mutations

--- composedb/frontend/pages/index.js
+++ composedb/frontend/pages/index.js
@@ -1,9 +1,25 @@
 import Head from "next/head"
-import { init, loadProfile } from "@/utils/database"
+import {
+  loadSession,
+  createProfile,
+  loadProfile,
+  logout,
+} from "@/utils/database"
 
 export default function Home() {
-  function initDatabase() {
-    init()
+  function loginToDb() {
+    loadSession()
+  }
+
+  function logoutFromDb() {
+    logout()
+  }
+
+  function create() {
+    createProfile({ name: "Kay", bio: "I am a technical writer!" })
+  }
+
+  function load() {
     loadProfile()
   }
 
@@ -13,7 +29,11 @@ export default function Home() {
         <title>ComposeDB Blog</title>
       </Head>
       <h1>ComposeDB Blog</h1>
-      <button onClick={initDatabase}>Init Database</button>
+      <button onClick={loginToDb}>login</button>
+      <button onClick={logoutFromDb}>logout</button>
+      <hr></hr>
+      <button onClick={create}>create</button>
+      <button onClick={load}>load</button>
     </>
   )
 }
--- composedb/frontend/utils/database.js
+++ composedb/frontend/utils/database.js
@@ -2,46 +2,92 @@ import { ComposeClient } from "@composedb/client"
 import { definition } from "../../composites/blog.runtime"
 import { DIDSession } from "did-session"
 import { EthereumWebAuth, getAccountId } from "@didtools/pkh-ethereum"
-import { ethers } from "ethers"
 
 let compose
+let session
 
-export function init() {
-  compose = new ComposeClient({
-    ceramic: `https://kay-is-curly-space-spork-rx94w5xqrq254pw-7007.preview.app.github.dev/`,
-    definition,
+const login = async () => {
+  const ethProvider = window.ethereum
+  const addresses = await ethProvider.request({
+    method: "eth_requestAccounts",
   })
-  console.log(compose)
-}
-
-export async function login() {
-  const ethProvider = ethers.getDefaultProvider()
-  const addresses = await ethProvider.request({ method: "eth_requestAccounts" })
   const accountId = await getAccountId(ethProvider, addresses[0])
-  const authMethod = await EthereumWebAuth.getAuthMethod(ethprovider, accountId)
+  const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)
 
-  const { did } = await DIDSession.authorize(authMethod, {
+  const session = await DIDSession.authorize(authMethod, {
     resources: compose.resources,
   })
-  compose.setDID(did)
+
+  localStorage.setItem("didsession", session.serialize())
+
+  return session
+}
+
+export const logout = () => {
+  compose = null
+  session = null
+  localStorage.removeItem("didsession")
+  console.log("Logged out!")
+}
+
+export const loadSession = async () => {
+  console.log("Checking active session...")
+  if (session && !session.isExpired) return console.log("Active session found!")
+
+  console.log("No active session found! Loading from localstorage...")
+
+  const sessionStr = localStorage.getItem("didsession")
+
+  if (sessionStr) {
+    session = await DIDSession.fromSession(sessionStr)
+    console.log("Session loaded!")
+  }
+
+  compose = new ComposeClient({
+    ceramic:
+      "https://kay-is-curly-space-spork-rx94w5xqrq254pw-7007.preview.app.github.dev/",
+    definition,
+  })
+
+  if (!session || (session.hasSession && session.isExpired)) {
+    console.log("Session not found or expired! Starting login...")
+    session = await login()
+  }
+
+  compose.setDID(session.did)
+  console.log("Session active!", session)
+
+  return compose
 }
 
 export async function loadProfile() {
+  const compose = await loadSession()
+
   const result = await compose.executeQuery(`
   {
     viewer {
       id
-      profile {
-        id
-        name
-        bio
-        image
-      }
+      profile { id name bio image }
     }
   }
   `)
 
-  console.log(result)
+  return result.data
+}
 
-  return result
+export async function createProfile({ name, bio, image }) {
+  const compose = await loadSession()
+
+  const result = await compose.executeQuery(
+    `
+    mutation CreateNewProfile($i: CreateProfileInput!) {
+      createProfile(input: $i) {
+        document { id name bio image }
+      }
+    }
+  `,
+    { i: { content: { name, bio, image } } }
+  )
+
+  return result.data
 }
